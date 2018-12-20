07194b68232a5ff2b4941cf4183f280ded2f391b
AhmedTiko96 <a7madtarek96@gmail.com>
2018-12-20 18:38:49 +0100

    modified start/end  points configuration

--- scripts/path_points_processor.py
+++ scripts/path_points_processor.py
@@ -7,10 +7,11 @@ import networkx as nx
 from nav_msgs.msg import Path
 from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import NavSatFix
+from shapely.geometry import Point
 
 import time
+import math
 import utm
-import cPickle as pickle
 import os
 
 class path_processing_planning:
@@ -66,19 +67,18 @@ class path_processing_planning:
 			try:
 
 				rospy.loginfo("First Downloading Attempt")
-				self._graph = ox.graph_from_place(name, network_type='all')
+				self._graph = ox.graph_from_place(name, network_type='drive')
 				self._graph_proj = ox.project_graph(self._graph)
-				ox.save_graphml(self._graph_proj ,folder= file_path_map ,  filename=name +'.xml')
+				ox.save_graphml(self._graph_proj ,folder= file_path_map , filename=name +'.xml')
 			except:
 				rospy.loginfo("First Downloading Attempt Failed, Retrying Download")
 				self._graph = ox.graph_from_address(name, distance=250, network_type='drive')
 				self._graph_proj = ox.project_graph(self._graph)
 				ox.save_graphml(self._graph_proj ,folder= file_path_map ,  filename=name +'.xml')
-			
+		rospy.loginfo("Map Loaded Successfully")
 
 
 		self._nodes, self._edges = ox.graph_to_gdfs(self._graph_proj, nodes=True, edges=True)
-		rospy.loginfo("loaded")	
 
 	def plan_path(self):
 
@@ -87,6 +87,9 @@ class path_processing_planning:
 
 		# self._endx = 434763
 		# self._endy = 4464870
+
+		# self._endx = 442028.32		//retiro park
+		# self._endy = 4474037.16    //retiro park
 
 
 		self._origin=(self._starty,self._startx)
@@ -99,6 +102,21 @@ class path_processing_planning:
 		self._destination_node = ox.get_nearest_node(self._graph_proj, self._destination, method= 'euclidean')
 		self._route = nx.dijkstra_path(G= self._graph_proj, source= self._origin_node,
 		 target=self._destination_node , weight='length')
+
+		for j in range(0, len(self._edges)):
+			
+			if ((self._edges.u[j] == self._route[0]) and 
+				(self._edges.v[j] == self._route[1])):
+				
+				print("first edge")
+				# print(self._edges.geometry[j])
+				self._first_edge_geom = self._edges.geometry[j]
+			elif((self._edges.u[j] == self._route[len(self._route)-2]) and 
+				(self._edges.v[j] == self._route[len(self._route)-1])):
+				
+				print("last edge")
+				# print(self._edges.geometry[j])
+				self._last_edge_geom = self._edges.geometry[j]
 		
 	def generate_path_points(self):
 
@@ -109,23 +127,46 @@ class path_processing_planning:
 						self._route_pointx.append(self._edges.geometry[i].xy[0][g])
 						self._route_pointy.append(self._edges.geometry[i].xy[1][g])
 		
+
+		self._projected_start_point= self._first_edge_geom.interpolate(
+			self._first_edge_geom.project(Point(self._startx, self._starty)))
+
+		print(self._projected_start_point)
+
+		self._projected_end_point=self._last_edge_geom.interpolate(
+			self._last_edge_geom.project(Point(self._endx, self._endy)))
+
+		print(self._projected_end_point)
+
 		for i in range (0, len(self._route_pointx)):
-			
+
 			pose_st = PoseStamped()
 			pose_st.header.stamp=rospy.Time.now()
 			pose_st.header.seq=i
 
-		 	pose_st.pose.position.x = self._route_pointx[i]
-		 	pose_st.pose.position.y = self._route_pointy[i]
+			if i == 0:
+				pose_st.header.stamp=rospy.Time.now()
+			 	pose_st.pose.position.x = self._projected_start_point.x
+			 	pose_st.pose.position.y = self._projected_start_point.y
+			elif i == len(self._route_pointx):
+				pose_st.header.stamp=rospy.Time.now()
+				pose_st.pose.position.x = self._projected_end_point.x
+				pose_st.pose.position.y = self._projected_end_point.y
+			else:
+				pose_st.header.stamp=rospy.Time.now()
+				pose_st.pose.position.x = self._route_pointx[i]
+				pose_st.pose.position.y = self._route_pointy[i]
 
 		 	self._path.poses.append(pose_st)
+
+
 		self._old_UTMx=self._start_UTMx
 		self._old_UTMy=self._start_UTMy
-		# ox.plot_graph_route(self._graph_proj,self._route,route_linewidth=6 ,save=True)
 		self.curr_gps = rospy.Subscriber('/fix', NavSatFix,self.draw_subgraph)
 
 	def draw_subgraph(self,curr_gps):
 
+		
 		
 		self.curr=curr_gps
 
@@ -134,35 +175,36 @@ class path_processing_planning:
 
 		self._curr_lat=self.curr.latitude
 		self._curr_lon=self.curr.longitude
-		
+		if(math.isnan(self.curr.latitude)==False):
 			
-		self._curr_UTMx, self._curr_UTMy, _, _ = utm.from_latlon(self._curr_lat, self._curr_lon)
+			self._curr_UTMx, self._curr_UTMy, _, _ = utm.from_latlon(self._curr_lat, self._curr_lon)
 
 	
 
-		xdiff =self._curr_UTMx - self._old_UTMx
-		ydiff=self._curr_UTMy - self._old_UTMy
+			xdiff =self._curr_UTMx - self._old_UTMx
+			ydiff=self._curr_UTMy - self._old_UTMy
 		
-		rospy.loginfo("Distance in x directon: " + str (abs(xdiff)))
-		rospy.loginfo("Distance in y directon: " + str (abs(ydiff)))
-		if (abs(xdiff) > 25 or abs(ydiff) > 25):
+			rospy.loginfo("Distance in x directon: " + str (abs(xdiff)))
+			rospy.loginfo("Distance in y directon: " + str (abs(ydiff)))
+			if (abs(xdiff) > 25 or abs(ydiff) > 25):
 				
-			rospy.loginfo("Generating subgraph")
-			self._curr_point=(self._curr_UTMy, self._curr_UTMx)
-			self._curr_node = ox.get_nearest_node(self._graph_proj, self._curr_point, method='euclidean')
-			self._old_UTMx=self._curr_UTMx
-			self._old_UTMy=self._curr_UTMy
-			subgraph1= nx.ego_graph(self._graph_proj,self._curr_node, radius=2, center=True, undirected=False, distance=None)
-			fig, ax =ox.plot_graph(subgraph1)
-			ox.save_graphml(subgraph1 , filename=self.file_path_subgraph + 'subgraph.xml')
+				rospy.loginfo("Generating subgraph")
+				self._curr_point=(self._curr_UTMy, self._curr_UTMx)
+				self._curr_node = ox.get_nearest_node(self._graph_proj, self._curr_point, method='euclidean')
+				self._old_UTMx=self._curr_UTMx
+				self._old_UTMy=self._curr_UTMy
+				subgraph1= nx.ego_graph(self._graph_proj,self._curr_node, radius=2, center=True, undirected=False, distance=None)
+				fig, ax =ox.plot_graph(subgraph1)
+				ox.save_graphml(subgraph1 , filename=self.file_path_subgraph + 'subgraph.xml')
 			
+			else:
+				rospy.loginfo("distance is less than 25 meters")
 		else:
-			rospy.loginfo("distance is less than 25 meters")
+			rospy.loginfo("nan signal")
 	def return_path(self, goal):
 		
 		place_name= 'leganes,Spain'
 		self.get_map(place_name)
-		rospy.loginfo("Map Loaded Successfully")
 		self.get_start()
 		self.get_end(goal)
 		self.plan_path()
