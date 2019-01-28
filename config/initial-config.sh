d2c30ddab43392c41f4b0a2627cd569d1ca6802f
mimiguel@ing.uc3m.es <mimiguel@ing.uc3m.es>
2019-01-28 14:51:38 +0100

    added config script

---
+++ config/initial-config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "---------------------------------------"
+echo "Setting up ROS OSM dependencies"
+echo "---------------------------------------"
+
+sudo apt-get install libspatialindex-dev
+sudo pip install osmnx
